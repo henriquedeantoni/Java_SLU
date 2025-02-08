2051df5ca394370e87324fb4352c75d498164cd4
henriquedeantoni <henriqdeantoni@gmail.com>
2025-02-07 23:58:44 -0300

    feat: nova classe Conversor

--- SLU/src/Modulo4/Conversor.java
+++ SLU/src/Modulo4/Conversor.java
@@ -1,9 +1,33 @@
 package Modulo4;
 
+
+
 public class Conversor {
 	
-	private String[] unidadesMetricas;
+	private String pergunta;
 	
-	private String[] unidadesInglesas;	
+	private String resposta;
 	
+	private String[] unCompMetricas;
+	
+	private String[] unCompInglesas;
+	
+	private String[] unMassaMetricas;
+	
+	private String[] unMassaInglesas;
+	
+	private String[] unVolumeMetricas;
+	
+	private String[] unVolumeInglesas;			
+	
+}
+
+public class PadraoMetricoSI
+{
+	private final double[] relCompMetricaUnidades = {1000, 100, 10,  1.00, 0.1, 0.01, 0.001};
+	private final String[] unCompMetricaUnidades = {"quilómetro", "hectômetro", "decâmetro", "metro", "decimetro", "centímrtro", "milimetro"};
+	
+	private final double relMassaMetrica = 1;
+	
+	private final double relVolumeMetrica = 1;
 }
