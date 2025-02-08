3fd4f080b472a101da66e8a074c11c74082c7d24
henriquedeantoni <henriqdeantoni@gmail.com>
2025-02-08 01:35:01 -0300

    refact: ajusta da classe

--- SLU/src/Modulo4/Conversor.java
+++ SLU/src/Modulo4/Conversor.java
@@ -18,16 +18,32 @@ public class Conversor {
 	
 	private String[] unVolumeMetricas;
 	
-	private String[] unVolumeInglesas;			
+	private String[] unVolumeInglesas;	
+	
+	
+	public class ComprimentoUnidades
+	{
+		private final double[] relCompMetricaUnidades = {1000, 100, 10,  1.000, 0.1, 0.01, 0.001};
+		private final String[] unCompMetricaUnidades = {"quilómetro", "hectômetro", "decâmetro", "metro", "decimetro", "centímetro", "milimetro"};
+	
+		private final double[] relCompInglesUnidades = {63360, 36, 12,  1.000, 0.001};
+		private final String[] unCompInglesUnidades = {"milha", "jarda", "pé", "polegada", "mil"};
+		
+		private final double relationComprMetricIngles = (1000/1609.344); // 1000 metro tem 1000/1609.344 milhas
+	}
+	
+	public class MassaUnidades
+	{
+		private final double[] relMassMetricaUnidades = {1000,  1.000, 0.001};
+		private final String[] unMassMetricaUnidades = {"tonelada", "quilograma", "grama"};
+	
+		private final double[] relCompInglesUnidades = {2000, 1.000, 0.0625};
+		private final String[] unCompInglesUnidades = {"ton", "libras", "onça"};
+		
+		private final double relationMassaMetricIngles = (1000/453.592); // 1.000 kg tem 1000/453.592 libras
+	}	
+	
 	
 }
 
-public class PadraoMetricoSI
-{
-	private final double[] relCompMetricaUnidades = {1000, 100, 10,  1.00, 0.1, 0.01, 0.001};
-	private final String[] unCompMetricaUnidades = {"quilómetro", "hectômetro", "decâmetro", "metro", "decimetro", "centímrtro", "milimetro"};
-	
-	private final double relMassaMetrica = 1;
-	
-	private final double relVolumeMetrica = 1;
-}
+
